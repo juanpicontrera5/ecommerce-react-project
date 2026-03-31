a586a65a377cc130b904ae22c8d4a4ae79cc3fa5
Juan Pablo Contreras <pablocontreras5478@gmail.com>
2026-03-31 15:51:29 -0400

    already moved everything

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { Routes, Route } from 'react-router'
 import { HomePage } from './pages/HomePage'
 import { CheckoutPage } from './pages/CheckoutPage'
+import { OrdersPage } from './pages/OrdersPage'
 
 import './App.css'
+import { TrackingPage } from './pages/TrackingPage'
 
 function App() {
 
@@ -10,7 +12,9 @@ function App() {
     <>
       <Routes>
         <Route path='/' element={<HomePage/>} />
-        <Route path="checkout" element={ <CheckoutPage/> } /> 
+        <Route path="checkout" element={ <CheckoutPage/> } />
+        <Route path='orders' element = { <OrdersPage/>} />
+        <Route path='tracking' element={<TrackingPage/>} />
       </Routes>
   
     </>
