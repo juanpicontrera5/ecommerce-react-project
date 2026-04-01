92b63358431ff74e8e238d4a5b2c983a1c20fe02
Juan Pablo Contreras <pablocontreras5478@gmail.com>
2026-04-01 02:04:52 -0400

    use data from the backend

--- ecommerce-frontend/src/pages/HomePage.jsx
+++ ecommerce-frontend/src/pages/HomePage.jsx
@@ -1,15 +1,21 @@
+import axios from 'axios';
+
+import { useEffect, useState } from 'react';
+
 import './HomePage.css'
 import { Header } from '../components/Header';
-import { products } from '../../starting-code/data/products'
 
 export function HomePage(){
 
-    fetch('http://localhost:3000/api/products')
-        .then((response)=>{
-            return response.json();
-        }).then((data) => {
-            console.log(data);
-        })
+    const[products, setProducts] = useState([])
+
+    useEffect(()=>{
+        axios.get('http://localhost:3000/api/products')
+            .then((response)=>{
+                setProducts (response.data)
+            })        
+    }, [])
+
 
     return (
     <>
