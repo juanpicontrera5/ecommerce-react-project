f4dd803d4fc795f043b459953eebbcc3134f2d79
Juan Pablo Contreras <pablocontreras5478@gmail.com>
2026-04-03 10:02:24 -0400

    lift car up and sharing between pages

--- ecommerce-frontend/src/App.jsx
+++ ecommerce-frontend/src/App.jsx
@@ -2,17 +2,30 @@ import { Routes, Route } from 'react-router'
 import { HomePage } from './pages/HomePage'
 import { CheckoutPage } from './pages/CheckoutPage'
 import { OrdersPage } from './pages/OrdersPage'
-
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css'
 import { TrackingPage } from './pages/TrackingPage'
 
 function App() {
+  
+  const[cart, setCart] = useState([]);
+  
+    useEffect(()=>{
+
+      axios.get('/api/cart-items?expand=product')
+        .then((response)=>{
+            setCart(response.data);
+      });
+
+    }, [])
+
 
   return (
     <>
       <Routes>
-        <Route path='/' element={<HomePage/>} />
-        <Route path="checkout" element={ <CheckoutPage/> } />
+        <Route path='/' element={<HomePage cart = {cart} />} />
+        <Route path="checkout" element={ <CheckoutPage cart = {cart} /> } />
         <Route path='orders' element = { <OrdersPage/>} />
         <Route path='tracking' element={<TrackingPage/>} />
       </Routes>
