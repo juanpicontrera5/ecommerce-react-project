7ce60c80ec0b0f22d0954f166304e9b651367759
juanpi <pablocontreras5478@gmail.com>
2026-04-16 14:06:08 -0400

    create delete cart item functions

--- ecommerce-frontend/src/pages/checkout/OrderSummary.jsx
+++ ecommerce-frontend/src/pages/checkout/OrderSummary.jsx
@@ -1,6 +1,7 @@
 import dayjs from "dayjs";
 import { DeliveryOption } from "./DeliveryOptions";
 import { formatMoney } from "../../utils/money";
+import axios from "axios";
 
 export function OrderSummary({ cart, deliveryOptions, loadCart }) {
     return (
@@ -10,6 +11,11 @@ export function OrderSummary({ cart, deliveryOptions, loadCart }) {
                     const selectedDeliveryOption = deliveryOptions.find((deliveryOption) => {
                         return deliveryOption.id === cartItem.deliveryOptionId;
                     })
+
+                    const deleteCartItem = async() => {
+                        await axios.delete(`/api/cart-items/${cartItem.productId}`);
+                        await loadCart();
+                    };
 
                     return (
                         <div
@@ -40,7 +46,9 @@ export function OrderSummary({ cart, deliveryOptions, loadCart }) {
                                         <span className="update-quantity-link link-primary">
                                             Update
                                         </span>
-                                        <span className="delete-quantity-link link-primary">
+                                        <span className="delete-quantity-link link-primary"
+                                            onClick={deleteCartItem}
+                                        >
                                             Delete
                                         </span>
                                     </div>
--- ecommerce-frontend/src/pages/checkout/PaymentSummary.jsx
+++ ecommerce-frontend/src/pages/checkout/PaymentSummary.jsx
@@ -1,54 +1,65 @@
-import { formatMoney } from "../../utils/money";
+import axios from 'axios';
+import { useNavigate } from 'react-router';
+import { formatMoney } from '../../utils/money';
 
-export function PaymentSummary( {paymentSummary} ){
+export function PaymentSummary({ paymentSummary, loadCart }) {
+  const navigate = useNavigate();
 
-    return(
-                    <div className="payment-summary">
-                        <div className="payment-summary-title">
-                            Payment Summary
-                        </div>
+  const createOrder = async () => {
+    await axios.post('/api/orders');
+    await loadCart();
+    navigate('/orders');
+  };
 
-                        {paymentSummary && (
-                            <>
-                                <div className="payment-summary-row">
-                                    <div>Items ({paymentSummary.totalItems}):</div>
-                                    <div className="payment-summary-money">
-                                        {formatMoney(paymentSummary.productCostCents)}
-                                    </div>
-                                </div>
+  return (
+    <div className="payment-summary">
+      <div className="payment-summary-title">
+        Payment Summary
+      </div>
 
-                                <div className="payment-summary-row">
-                                    <div>Shipping &amp; handling:</div>
-                                    <div className="payment-summary-money">
-                                        {formatMoney(paymentSummary.shippingCostCents)}
-                                    </div>
-                                </div>
+      {paymentSummary && (
+        <>
+          <div className="payment-summary-row">
+            <div>Items ({paymentSummary.totalItems}):</div>
+            <div className="payment-summary-money">
+              {formatMoney(paymentSummary.productCostCents)}
+            </div>
+          </div>
 
-                                <div className="payment-summary-row subtotal-row">
-                                    <div>Total before tax:</div>
-                                    <div className="payment-summary-money"> {formatMoney(paymentSummary.totalCostBeforeTaxCents)} </div>
-                                </div>
+          <div className="payment-summary-row">
+            <div>Shipping &amp; handling:</div>
+            <div className="payment-summary-money">
+              {formatMoney(paymentSummary.shippingCostCents)}
+            </div>
+          </div>
 
-                                <div className="payment-summary-row">
-                                    <div>Estimated tax (10%):</div>
-                                    <div className="payment-summary-money">
-                                        {formatMoney(paymentSummary.taxCents)}
-                                    </div>
-                                </div>
+          <div className="payment-summary-row subtotal-row">
+            <div>Total before tax:</div>
+            <div className="payment-summary-money">
+              {formatMoney(paymentSummary.totalCostBeforeTaxCents)}
+            </div>
+          </div>
 
-                                <div className="payment-summary-row total-row">
-                                    <div>Order total:</div>
-                                    <div className="payment-summary-money">
-                                        {formatMoney(paymentSummary.totalCostCents)}
-                                    </div>
-                                </div>
+          <div className="payment-summary-row">
+            <div>Estimated tax (10%):</div>
+            <div className="payment-summary-money">
+              {formatMoney(paymentSummary.taxCents)}
+            </div>
+          </div>
 
-                                <button className="place-order-button button-primary">
-                                    Place your order
-                                </button>
-                            </>
-                        )}
-                    </div>
-    );
+          <div className="payment-summary-row total-row">
+            <div>Order total:</div>
+            <div className="payment-summary-money">
+              {formatMoney(paymentSummary.totalCostCents)}
+            </div>
+          </div>
 
+          <button className="place-order-button button-primary"
+            onClick={createOrder}>
+            Place your order
+          </button>
+        </>
+      )}
+    </div>
+  );
 }
